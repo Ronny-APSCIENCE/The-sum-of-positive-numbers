9669f27cfa0a43c476e7439193f13adf6ce877b0
Ronny-APSCIENCE <ronnyillicachi@outlook.com>
2025-11-17 04:02:12 +0000

    finished_ronny_project.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def sum_of_positives(numbers):
+    total = 0
 
+    for x in numbers:
+        if x > 0:
+         total = total + x
+
+    return total
+
+my_list = [1, -2, 3, -4, 5]
+result = sum_of_positives(my_list)
+print("The result is ", result)
